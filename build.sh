526a8d7ab8a23da51f81176963b175c69eb576d0
Ben Black <ben.black@techcyte.com>
2025-04-02 22:10:17 -0600

    tested package!

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-#rm -rf build
+# rm -rf build
 mkdir -p build
 #(cd build && emconfigure cmake -DCMAKE_BUILD_TYPE=Debug ..) &&
-(cd build && emconfigure cmake ..) &&
-(cd build && emmake make VERBOSE=1 -j 16)
-cp ./build/src/libjpegturbowasm.js ./dist && 
-cp ./build/src/libjpegturbowasm.wasm ./dist && 
-cp ./build/src/libjpegturbojs.js.mem ./dist &&
-cp ./build/src/libjpegturbojs.js ./dist &&
+(cd build && emcmake cmake -DCMAKE_BUILD_TYPE=Release ..) &&
+(cd build && emmake make VERBOSE=1 -j 16) &&
+cp ./build/src/libjpegturbowasm.js ./dist &&
+cp ./build/src/libjpegturbowasm.wasm ./dist &&
 (cd test/node; npm run test)
