a6a4937c3779a039be2a897eb9cd58fb575a1556
Ben Black <ben.black@techcyte.com>
2025-04-08 19:50:22 -0600

    fixed cmakelists

--- build.sh
+++ build.sh
@@ -2,9 +2,37 @@
 # rm -rf build
 mkdir -p build
 #(cd build && emconfigure cmake -DCMAKE_BUILD_TYPE=Debug ..) &&
-(cd build && emcmake cmake -DCMAKE_BUILD_TYPE=Release ..) &&
-(cd build && emmake make VERBOSE=1 -j 16) &&
+emcc  -O3 -msse2 -msimd128  -c  \
+            -I extern/libjpeg-turbo -I src \
+         extern/libjpeg-turbo/turbojpeg.c \
+         extern/libjpeg-turbo/jdapimin.c \
+         extern/libjpeg-turbo/jcapimin.c \
+         extern/libjpeg-turbo/jcapistd.c \
+         extern/libjpeg-turbo/jdapistd.c \
+         extern/libjpeg-turbo/jdatadst-tj.c \
+         extern/libjpeg-turbo/jdatasrc-tj.c \
+         extern/libjpeg-turbo/jerror.c \
+         extern/libjpeg-turbo/jcparam.c \
+         extern/libjpeg-turbo/jmemmgr.c \
+         extern/libjpeg-turbo/jdmarker.c \
+         extern/libjpeg-turbo/jdinput.c \
+         extern/libjpeg-turbo/jcomapi.c \
+         extern/libjpeg-turbo/jutils.c \
+         extern/libjpeg-turbo/jmemnobs.c \
+         extern/libjpeg-turbo/jdmaster.c \
+         extern/libjpeg-turbo/cjpeg.c
+em++ -O3 -msse2 -msimd128   -c \
+         -I extern/libjpeg-turbo -I src \
+         src/jslib.cpp -o jslib.o
+echo "Objects finished"
+em++ -O3 -msse2 -msimd128 -lembind --emit-tsd ./libjpegturbowasm.d.ts   \
+       -O3           -msimd128           --bind        \
+         -s MODULARIZE=1          -s EXPORT_NAME=libjpegturbowasm          -s DISABLE_EXCEPTION_CATCHING=1          -s ASSERTIONS=0          -s NO_EXIT_RUNTIME=1          -s MALLOC=emmalloc          -s ALLOW_MEMORY_GROWTH=1          -s TOTAL_MEMORY=1073741824          -s FILESYSTEM=0          -s EXPORTED_FUNCTIONS=[]          -s EXPORTED_RUNTIME_METHODS=[ccall]          -s EXPORT_ES6=1          -s EXPORT_NAME=instantiate    \
+         -I extern/libjpeg-turbo -I src \
+         jslib.o turbojpeg.o jdmaster.o jmemnobs.o jdapimin.o jutils.o jcomapi.o jdinput.o jdmarker.o jmemmgr.o jdapistd.o jcapimin.o jdatasrc-tj.o jcparam.o jerror.o jdatadst-tj.o jcapistd.o cjpeg.o -o libjpegturbowasm.js
+# (cd build && emcmake cmake -DCMAKE_BUILD_TYPE=Release ..) &&
+# (cd build && emmake make VERBOSE=1 -j 16) &&
 cp ./build/src/libjpegturbowasm.js ./dist &&
 cp ./build/src/libjpegturbowasm.d.ts ./dist &&
-cp ./build/src/libjpegturbowasm.wasm ./dist &&
-(cd test/node; npm run test)
+cp ./build/src/libjpegturbowasm.wasm ./dist #&&
+#(cd test/node; npm run test)
---
+++ index.ts
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2020 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Notice: I (Jamie Sinclair) have copied this code from the JPEG encode module
+ * and modified it to decode JPEG images.
+ */
+
+import type TurboJPEGModule from "./dist/libjpegturbowasm.js";
+import { initEmscriptenModule } from "./utils.js";
+
+import mozjpeg_dec from "./codec/dec/mozjpeg_dec.js";
+import { DecodeOptions, defaultDecodeOptions } from "./meta.js";
+
+let emscriptenModule: Promise<MozJPEGModule>;
+
+export async function init(
+  module?: WebAssembly.Module,
+  moduleOptionOverrides?: Partial<EmscriptenWasm.ModuleOpts>
+): Promise<void> {
+  emscriptenModule = initEmscriptenModule(
+    mozjpeg_dec,
+    module,
+    moduleOptionOverrides
+  );
+}
+
+export default async function decode(
+  buffer: ArrayBuffer,
+  options: Partial<DecodeOptions> = {}
+): Promise<ImageData> {
+  if (!emscriptenModule) init();
+
+  const _options = { ...defaultDecodeOptions, ...options };
+  const module = await emscriptenModule;
+  const result = module.decode(buffer, _options.preserveOrientation);
+  if (!result) throw new Error("Decoding error");
+  return result;
+}
